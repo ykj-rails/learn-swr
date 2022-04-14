6cba53efdf7cef9c0a49f360106d3575ac70880c
松本雄揮 <yukiji.0307@gmail.com>
2022-04-14 18:06:03 +0900

    swr/mswのセットアップ諸々

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+type List = {
+  name: string
+  job: string
+  age: number
+}
 
 export const Header = () => {
+  const [list, setList] = useState<List | null>(null)
+  console.log(list)
+
+  const fetchListData = async (setList: any) => {
+    const res = await fetch('/list')
+    const listData = await res.json()
+    setList(listData)
+  }
+
+  useEffect(() => {
+    fetchListData(setList)
+  }, [])
+
+  console.log(list)
   return <header>Header</header>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser')
+  // const { worker } = await import('./mocks/browser')
+  worker.start()
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
