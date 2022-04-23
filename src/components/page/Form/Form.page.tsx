906b8b98a493f5644a114b0204dccf1fea459187
ykj-rails <yukiji.0307@gmail.com>
2022-04-23 16:00:52 +0900

    [update] Form Component / Page 作成

---
+++ src/components/page/Form/Form.page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Footer, Header } from '@/components/common'
+
+import { Form } from './Form'
+
+export const FormPage = () => {
+  return (
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
+  )
+}
---
+++ src/pages/form/index.tsx
@@ -0,0 +1,3 @@
+import { FormPage } from '@/components/page/Form'
+
+export default FormPage
